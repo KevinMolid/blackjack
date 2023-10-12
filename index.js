e0ae6aa05f3d67449fa4854c4fe3348e49178120
Kevin Molid <kevinmolid@gmail.com>
2023-10-12 22:05:35 +0200

    Added dealer

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // Initiate cards
 let cards = [] // array - ordered list of items
+let dealerCards = []
 let sum = 0
+let dealerSum = 0
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let dealerSumEl = document.getElementById("dealer-sum-el")
+let dealerCardsEl = document.getElementById("dealer-cards-el")
+
+
+// Create the player object
+let player = {
+  name: "Kevin",
+  chips: 165
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 let hasBlackJack = false
 let isAlive = false
@@ -22,20 +36,34 @@ function getRandomCard() {
 }
 
 function startGame() {
+  // player
   isAlive = true
   let firstCard = getRandomCard()
   let secondCard = getRandomCard()
   cards = [firstCard, secondCard]
   sum = firstCard + secondCard
+  // dealer
+  let dealerFirstCard = getRandomCard()
+  let dealerSecondCard = getRandomCard()
+  dealerCards = [dealerFirstCard, dealerSecondCard]
+  dealerSum = dealerFirstCard + dealerSecondCard
   renderGame()
 }
 
 function renderGame() {
+  // player
   sumEl.textContent = "Sum: " + sum
   cardsEl.textContent = "Cards: "
   for (let i = 0; i < cards.length; i++) {
     cardsEl.textContent += cards[i] + " "
   }
+  //dealer
+  dealerSumEl.textContent = "Sum: " + dealerSum
+  dealerCardsEl.textContent = "Cards: "
+  for (let i = 0; i < dealerCards.length; i++) {
+    dealerCardsEl.textContent += dealerCards[i] + " "
+  }
+  // message
   if (sum < 21) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
